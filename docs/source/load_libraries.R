767e99ca506cf714a0c2b05c6fbdf58b81e9b5e5
Marcel Raab <marcel.raab@gmail.com>
2022-02-02 09:06:35 +0100

    Added Chapter 2.5

--- docs/source/load_libraries.R
+++ docs/source/load_libraries.R
@@ -9,8 +9,8 @@
 pkgs <- c("Cairo", "cluster", "colorspace", "corrplot", "fontawesome", "glue", 
           "grDevices", "gt", "haven", "Hmisc", "htmltools", "icons",
           "knitr", "kableExtra", "magrittr", "metathis", "OpenMx", 
-          "patchwork", "pdftools", "RColorBrewer", "sjPlot", "summarytools",
-          "tidyverse", "TraMineR", "TraMineRextras", 
+          "patchwork", "pdftools", "RColorBrewer", "seqquality", "sjPlot", 
+          "summarytools", "tidyverse", "TraMineR", "TraMineRextras", 
           "tufte", "reshape2", "WeightedCluster", "xaringanExtra") 
 
 
