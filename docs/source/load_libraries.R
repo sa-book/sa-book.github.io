42760ed1d6cff0f06c3a934c53c1a3817246695e
Marcel Raab <marcel.raab@gmail.com>
2022-04-06 19:19:33 +0200

    Added ggplot bonus material

--- docs/source/load_libraries.R
+++ docs/source/load_libraries.R
@@ -7,8 +7,8 @@
 
 # list of required libraries 
 pkgs <- c("broom", "Cairo", "cluster", "colorspace", "corrplot", "descr", "effects", 
-          "factoextra", "fontawesome", "glue", 
-          "grDevices", "gt", "haven", "Hmisc", "htmltools", "icons",
+          "factoextra", "fontawesome", "ggseqplot", "glue", 
+          "grDevices", "gt", "haven", "Hmisc", "hrbrthemes", "htmltools", "icons",
           "knitr", "kableExtra", "magrittr", "metathis", "nnet", "OpenMx", 
           "patchwork", "pdftools", "psych", "RColorBrewer", "seqquality", "sjPlot", 
           "stats", "summarytools", "tidyr", "tidyverse", "TraMineR", "TraMineRextras", 
